e7c32c4bccbf8b8622d0df0df94f093ce359e194
Saloni Patil <salonivpatil2000@gmail.com>
2020-10-16 09:00:13 +0530

    second commit

--- login.php
+++ login.php
@@ -1,48 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-
-    <!-- Bootstrap & CSS -->
-    <link rel="stylesheet" href="">
-
-    <link rel="stylesheet" href="style.css">
-
-    <title>Login</title>
+    <title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-
 <body>
-    
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 offset-md-4 form.div login">
-              <form action:"signup.php" method="post">
-                 <h3 class="text-center">Login</h3>
-
-                <!--<div class="alert alert-danger">
-                   <li>Username required</li>
-                </div>-->
-
-                 <div class="form-group">
-                    <label for="username">Username or Email</label>
-                    <input type="text" name="username" class="form-control form-control-lg">
-                 </div>
-
-                 <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="passsword" name="password" class="form-control form-control-lg">
-                 </div>
-
-                 <div class="form-group">
-                    <button type="submit" name="login.btn" class="btn btm-primary btn-block btn-lg">Login</button>
-                 </div>
-                 <p class="text-cemter">Already a member? <a href="signup.php">Sign Up</a></p>
-
-                </form>    
-
-            </div>
-        </div>
-    </div>
+    <div id="frm">
+       <form action="process.php" method="POST">
+         <p>
+            <label>Username:</label>
+            <input type="text" id="user" name="user" />
+         </p>
+         <p>
+            <label>Password:</label>
+            <input type="password" id="pass" name="pass" />
+         </p>	 
+		 <p>
+		    <input type="submit" id="btn" value="Login" />
+		 </p>
+		 
+	   </form>
+	</div>
+	
 </body>
-</html>
+</html>	
+		
